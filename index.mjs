d9763de2634e2c4d39a0659c8a1c98fd750d2d26
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-06-11 16:34:19 +0200

    implement paseUsernames function

--- index.mjs
+++ index.mjs
@@ -1,3 +1,12 @@
 export function makeFetchRequest() {
   return fetch("https://example.com/test");
 }
+
+export function parseUsernames(usernames) {
+  const usernameArray = usernames
+    .split(",")
+    .map((username) => username.trim())
+    .filter((username) => username.length > 0);
+
+  return [...new Set(usernameArray)];
+}
