e89d6337f57f7c2a48e66d4b3408e48e86dc2472
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-06-11 16:17:38 +0200

    remove handle submit form test

--- index.test.mjs
+++ index.test.mjs
@@ -1,7 +1,7 @@
 import { describe, test } from "node:test";
 import assert from "node:assert";
 import nock from "nock";
-import { makeFetchRequest, handleUsernameFormSubmit } from "./index.mjs";
+import { makeFetchRequest } from "./index.mjs";
 
 test("mocks a fetch function", async () => {
   const scope = nock("https://example.com")
@@ -13,20 +13,4 @@ test("mocks a fetch function", async () => {
   assert(parsedResponse.user === "someone");
 
   assert(scope.isDone() === true, "No matching fetch request has been made");
-});
-
-describe("handleUsernameFormSubmit", () => {
-  test("prevents the default form submission behaviour", () => {
-    const event = {
-      preventDefault: () => {
-        event.preventDefaultCalled = true;
-      },
-    };
-
-    handleUsernameFormSubmit(event);
-    assert(
-      event.preventDefaultCalled === true,
-      "preventDefault was not called",
-    );
-  });
 });
